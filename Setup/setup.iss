ce5d3ea1658a8268c6c520959c30e62a253d7fc3
runaum <alexey.naumov@gmail.com>
2013-04-14 06:46:18 +0600

    [+] Initial upload (original sources by 02.04.2013)

---
+++ Setup/setup.iss
@@ -0,0 +1,62 @@
+[Setup]
+AppName=SynWrite
+AppVersion=5.1.050
+AppPublisher=UVViewSoft
+AppPublisherURL=http://uvviewsoft.com
+AppSupportURL=http://uvviewsoft.com
+DefaultDirName={pf}\SynWrite
+DefaultGroupName=SynWrite
+DisableProgramGroupPage=yes
+OutputDir=D:\S
+OutputBaseFilename=SynWrite
+SetupIconFile=D:\S\Ico\Syn_mainicon.ico
+Compression=lzma
+SolidCompression=yes
+
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+Name: "german"; MessagesFile: "compiler:Languages\German.isl"
+Name: "russian"; MessagesFile: "compiler:Languages\Russian.isl"
+Name: "spanish"; MessagesFile: "compiler:Languages\Spanish.isl"
+
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"
+
+[Files]
+Source: "..\Syn.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\CP.cfg"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\Lexers.cfg"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\Tidy.cfg"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\HL\*.acp"; DestDir: "{app}\HL"; Flags: ignoreversion
+Source: "..\HL\*.lxl"; DestDir: "{app}\HL"; Flags: ignoreversion confirmoverwrite
+Source: "..\Lang\*"; DestDir: "{app}\Lang"; Flags: ignoreversion
+Source: "..\Readme\*"; DestDir: "{app}\Readme"; Flags: ignoreversion
+Source: "..\Template\colors\*"; DestDir: "{app}\Template\colors"; Flags: ignoreversion
+Source: "..\Template\newdoc\*"; DestDir: "{app}\Template\newdoc"; Flags: ignoreversion
+Source: "..\Template\tools\*"; DestDir: "{app}\Template\tools"; Flags: ignoreversion
+Source: "..\Template\clips\*"; DestDir: "{app}\Template\clips"; Flags: ignoreversion
+Source: "..\Tools\*"; DestDir: "{app}\Tools"; Flags: ignoreversion
+Source: "..\Dictionaries\*"; DestDir: "{app}\Dictionaries"; Flags: ignoreversion
+Source: "..\SynPlugins.sample.ini"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\OfficeTheme.sample.ini"; DestDir: "{app}"; Flags: ignoreversion
+Source: "..\Plugins\Explorer\*"; DestDir: "{app}\Plugins\Explorer"; Flags: ignoreversion
+Source: "..\Plugins\SynFTP\*"; DestDir: "{app}\Plugins\SynFTP"; Flags: ignoreversion
+Source: "..\Plugins\FindIDPas\*"; DestDir: "{app}\Plugins\FindIDPas"; Flags: ignoreversion
+
+[Icons]
+Name: "{group}\SynWrite"; Filename: "{app}\Syn.exe"
+Name: "{group}\SynWrite Help"; Filename: "{app}\Readme\SynWrite.chm"
+Name: "{group}\{cm:UninstallProgram,SynWrite}"; Filename: "{uninstallexe}"
+Name: "{commondesktop}\SynWrite"; Filename: "{app}\Syn.exe"; Tasks: desktopicon
+
+[Run]
+Filename: "{app}\Syn.exe"; Parameters: "/reg"; Description: "Configure SynWrite file types"; Flags: nowait postinstall skipifsilent runascurrentuser
+;;Filename: "{app}\Readme\History.txt"; Description: "View versions history"; Flags: nowait shellexec postinstall skipifsilent
+
+[UninstallDelete]
+Type: files; Name: "{app}\Syn*.ini"
+
+[Registry]
+Root: HKCR; Subkey: "*\shell\SynWrite"; Flags: uninsdeletekey
+Root: HKCR; Subkey: "*\shell\SynWrite\command"; Flags: uninsdeletekey
+Root: HKCR; Subkey: "*\shell\SynWrite\command"; ValueType: string; ValueName: ""; ValueData: """{app}\Syn.exe"" ""%1"""
---
+++ unTool2.pas
@@ -0,0 +1,153 @@
+unit unTool2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, TntStdCtrls, Tntforms, DKLang, ExtCtrls, Menus,
+  TntMenus, tntClasses;
+
+type
+  TfmToolOutput = class(TTntForm)
+    bOk: TTntButton;
+    bCancel: TTntButton;
+    TntGroupBox1: TTntGroupBox;
+    TntLabel1: TTntLabel;
+    TntLabel2: TTntLabel;
+    edRE: TTntEdit;
+    TntLabel3: TTntLabel;
+    TntLabel4: TTntLabel;
+    edFN: TTntComboBox;
+    edLn: TTntComboBox;
+    edCol: TTntComboBox;
+    TntLabel5: TTntLabel;
+    DKLanguageController1: TDKLanguageController;
+    TntLabel6: TTntLabel;
+    edTest: TTntEdit;
+    bTest: TTntButton;
+    Bevel1: TBevel;
+    bPre: TTntButton;
+    Bevel2: TBevel;
+    mnuPreset: TTntPopupMenu;
+    cbNoTag: TTntCheckBox;
+    procedure TntFormCreate(Sender: TObject);
+    procedure bTestClick(Sender: TObject);
+    procedure bPreClick(Sender: TObject);
+    procedure mnuPresetPopup(Sender: TObject);
+    procedure TntFormDestroy(Sender: TObject);
+    procedure edTestChange(Sender: TObject);
+    procedure TntFormShow(Sender: TObject);
+  private
+    { Private declarations }
+    FList: TTntStringList;
+    procedure MnuClick(Sender: TObject);
+  public
+    { Public declarations }
+  end;
+
+var
+  fmToolOutput: TfmToolOutput;
+
+implementation
+
+uses unProc, ATxFProc, inifiles;
+
+{$R *.dfm}
+
+procedure TfmToolOutput.TntFormCreate(Sender: TObject);
+var i:Integer;
+  sd:Widestring;
+begin
+  with edFN.Items do begin Add('--'); for i:= 1 to 8 do Add(Inttostr(i)); end;
+  with edLn.Items do begin Add('--'); for i:= 1 to 8 do Add(Inttostr(i)); end;
+  with edCol.Items do begin Add('--'); for i:= 1 to 8 do Add(Inttostr(i)); end;
+
+  FList:= TTntStringlist.Create;
+  sd:= ExtractFileDir(GetModuleName(HInstance))+'\Template\tools';
+  FFindToList(FList, sd, '*.tool', false, false, false, false);
+  FList.Sort;
+end;
+
+procedure TfmToolOutput.bTestClick(Sender: TObject);
+var
+  fn: Widestring;
+  n_line, n_col:Integer;
+begin
+  fn:= '';
+  SParseOut(edTest.Text, edRE.Text,
+    edFN.ItemIndex, edLn.ItemIndex, edCol.ItemIndex, fn, n_line, n_col);
+
+  if fn='' then begin Msg(DKLangConstW('O_fn')); Exit end;
+  if n_line<=0 then begin Msg(DKLangConstW('O_l')); Exit end;
+
+  MsgInfo(WideFormat(DKLangConstW('O_ok'), [fn, n_line, n_col]));
+end;
+
+procedure TfmToolOutput.bPreClick(Sender: TObject);
+var p:TPoint;
+begin
+  p:= Point(0, bPre.Height);
+  p:= bPre.ClientToScreen(p);
+  MnuPreset.Popup(p.x, p.y);
+end;
+
+function MenuItemName(const fn:string):string;
+begin
+  with TIniFile.Create(fn) do
+  try
+    Result:= Format('%s'#9'(%s)',
+      [ReadString('Tool', 'name', ''),
+       ReadString('Tool', 'exe', '')]);
+  finally
+    Free
+  end;
+end;
+
+procedure TfmToolOutput.mnuPresetPopup(Sender: TObject);
+var
+  i: Integer;
+  m: TMenuItem;
+begin
+  with mnuPreset.Items do
+  begin
+    Clear;
+    for i:= 0 to FList.Count-1 do
+    begin
+      m:= TMenuItem.Create(Self);
+      m.Caption:= MenuItemName(FList[i]);
+      m.Tag:= i;
+      m.OnClick:= MnuClick;
+      Add(m);
+    end;
+  end;
+end;
+
+procedure TfmToolOutput.TntFormDestroy(Sender: TObject);
+begin
+  FreeAndNil(FList);
+end;
+
+procedure TfmToolOutput.MnuClick(Sender: TObject);
+begin
+  with TIniFile.Create(FList[TMenuItem(Sender).Tag]) do
+  try
+    edRE.Text:= ReadString('Regex', 's', '');
+    edFN.ItemIndex:= ReadInteger('Regex', 'fn', 0);
+    edLn.ItemIndex:= ReadInteger('Regex', 'line', 0);
+    edCol.ItemIndex:= ReadInteger('Regex', 'col', 0);
+  finally
+    Free
+  end;
+end;
+
+procedure TfmToolOutput.edTestChange(Sender: TObject);
+begin
+  bTest.Enabled:= edTest.Text<>'';
+end;
+
+procedure TfmToolOutput.TntFormShow(Sender: TObject);
+begin
+  edTestChange(Self);
+end;
+
+end.
